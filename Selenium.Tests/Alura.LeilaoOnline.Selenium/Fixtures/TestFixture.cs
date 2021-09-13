e88098c968bbb9cffcdfd9d4dd83d34685025165
Tiago Silva <tiagodasilva360@hotmail.com>
2021-09-13 11:52:44 -0300

    Inicio do versionamento

---
+++ Selenium.Tests/Alura.LeilaoOnline.Selenium/Fixtures/TestFixture.cs
@@ -0,0 +1,27 @@
+﻿using Alura.LeilaoOnline.Selenium.Helpers;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alura.LeilaoOnline.Selenium.Fixtures
+{
+    public class TestFixture : IDisposable
+    {
+        public IWebDriver Driver { get; private set; }
+
+        //Setup
+        public TestFixture()
+        {
+            Driver = new FirefoxDriver(TestHelper.PastaDoExecutavel);
+        }
+
+        //TearDown
+        public void Dispose()
+        {
+            Driver.Quit();
+        }
+    }
+}
