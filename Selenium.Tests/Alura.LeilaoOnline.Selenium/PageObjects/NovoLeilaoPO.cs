6bb3924bbdb4222ec9ec8425cb4292d8a89d00f4
Tiago Silva <tiagodasilva360@hotmail.com>
2021-09-13 19:27:47 -0300

    Formulários complexos

---
+++ Selenium.Tests/Alura.LeilaoOnline.Selenium/PageObjects/NovoLeilaoPO.cs
@@ -0,0 +1,78 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Alura.LeilaoOnline.Selenium.PageObjects
+{
+    public class NovoLeilaoPO
+    {
+        private IWebDriver driver;
+
+        private By byInputTitulo;
+        private By byInputDescricao;
+        private By byInputCategoria;
+        private By byInputValorIncial;
+        private By byInputImagem;
+        private By byInputIniciopregao;
+        private By byInputTerminoPregao;
+        private By byBotaoSalvar;
+        private By byInputCategoriaId;
+
+        public NovoLeilaoPO(IWebDriver driver)
+        {
+            this.driver = driver;
+            byInputTitulo = By.Id("Titulo");
+            byInputDescricao = By.Id("Descricao");
+            byInputCategoria = By.XPath("//*[contains(@class,'dropdown-content select-dropdown')]/li[2]/span");
+            byInputValorIncial = By.Id("ValorInicial");
+            byInputImagem = By.Id("ArquivoImagem");
+            byInputIniciopregao = By.Id("InicioPregao");
+            byInputTerminoPregao = By.Id("TerminoPregao");
+            byBotaoSalvar = By.CssSelector("button[type=submit]");
+            byInputCategoriaId = By.Id("Categoria");
+        }
+
+        public IEnumerable<string> Categorias
+        {
+            get
+            {
+                var elementoCategoria = new SelectElement(driver.FindElement(byInputCategoriaId));
+                return elementoCategoria.Options
+                    .Where(o => o.Enabled)
+                    .Select(o => o.Text);
+            }
+
+        }
+
+        public void Visitar()
+        {
+            driver.Navigate().GoToUrl("http://localhost:5000/Leiloes/Novo");
+        }
+
+        public void PreencheFormulario(
+            string titulo,
+            string descricao,
+            string categoria,
+            double valor,
+            string imagem,
+            DateTime inicio,
+            DateTime termino)
+        {
+            driver.FindElement(byInputTitulo).SendKeys(titulo);
+            driver.FindElement(byInputDescricao).SendKeys(descricao);
+            driver.FindElement(By.ClassName("select-wrapper")).Click();
+            driver.FindElement(byInputCategoria).Click();
+            driver.FindElement(byInputValorIncial).SendKeys(valor.ToString());
+            driver.FindElement(byInputImagem).SendKeys(imagem);
+            driver.FindElement(byInputIniciopregao).SendKeys(inicio.ToString("dd/MM/yyyy"));
+            driver.FindElement(byInputTerminoPregao).SendKeys(termino.ToString("dd/MM/yyyy"));
+        }
+
+        public void SubmeteFormulario()
+        {
+            driver.FindElement(byBotaoSalvar).Click();
+        }
+    }
+}
---
+++ Selenium.Tests/Alura.LeilaoOnline.Selenium/Testes/AoCriarLeilao.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using OpenQA.Selenium;
+using Xunit;
+using Alura.LeilaoOnline.Selenium.PageObjects;
+using Alura.LeilaoOnline.Selenium.Fixtures;
+using System.Threading;
+
+namespace Alura.LeilaoOnline.Selenium.Testes
+{
+    [Collection("Chrome Driver")]
+    public class AoCriarLeilao
+    {
+        private IWebDriver driver;
+
+        public AoCriarLeilao(TestFixture fixture)
+        {
+            driver = fixture.Driver;
+        }
+
+        [Fact]
+        public void DadoLoginAdminEInfoValidasDeveCadastrarLeilao()
+        {
+            //Arrange
+            var loginPO = new LoginPO(driver);
+            loginPO.Visitar();
+            loginPO.PreencheFormulario("admin@example.org", "123");
+            loginPO.SubmeteFormulario();
+
+            var novoLeilaoPO = new NovoLeilaoPO(driver);
+            novoLeilaoPO.Visitar();
+            novoLeilaoPO.PreencheFormulario(
+                "Leilão de Coleção 1",
+                "Teste",
+                "Item de Colecionador",
+                4000,
+                "C:\\Users\\tiago\\OneDrive\\Imagens\\Planet9\\Planet9_3840x2160.jpg",
+                DateTime.Now.AddDays(20),
+                DateTime.Now.AddDays(40)
+            );
+
+            //Act
+            novoLeilaoPO.SubmeteFormulario();
+
+            //Assert
+            Assert.Contains("Leilões cadastrados no sistema", driver.PageSource);
+        }
+    }
+}
